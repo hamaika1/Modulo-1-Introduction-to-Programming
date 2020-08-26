8d4365c447485d804fe41c1d32dfa2672c1311ac
Hamaika1 <hamaika9@gmail.com>
2020-08-26 18:02:11 -0300

    POO 2

--- Modulo 1/Ejercicios Net Beans/Net Beans Projects/pyCuentaBancaria/src/ar/com/Ejecutable.java
+++ Modulo 1/Ejercicios Net Beans/Net Beans Projects/pyCuentaBancaria/src/ar/com/Ejecutable.java
@@ -8,25 +8,36 @@ import java.util.Scanner;
  * @author mateo
  */
 public class Ejecutable {
-    
+
     public static void main(String[] args) {
-        
+
         Scanner input = new Scanner(System.in);
+
+        CuentaBancaria cuenta1 = new CuentaBancaria("Juan Perez", 1000, 1);
+        CuentaBancaria cuenta2 = new CuentaBancaria("Mateo Plante", 2000, 2);
         
-        CuentaBancaria cuenta = new CuentaBancaria();
-        
-        System.out.println("Ingrese en titular de la cuenta: ");
-        cuenta.setTitular(input.nextLine());
-        
-        System.out.println("Ingrese el saldo de la cuenta: ");
-        cuenta.setSaldo(input.nextFloat());
-        
-        System.out.println("Ingrese el codigo de la cuenta: ");
-        cuenta.setIdCodigo(input.nextInt());
-        
-        System.out.println("Titular de la cuenta: " + cuenta.getTitular());
-        System.out.println("Saldo de la cuenta: " + cuenta.getSaldo());
-        System.out.println("Codigo de la cuenta: " + cuenta.getIdCodigo());
+        //Prueba de deposito y extraccion;
+        System.out.println("Porfavor ingrese el monto a depositar en la cuenta codigo 1: ");
+        cuenta1.depositar(input.nextFloat());
+
+        System.out.println("Porfavor ingrese el montro a extraer de la cuenta codigo 1: ");
+        cuenta1.extraer(input.nextFloat());
+
+        //Muestro los datos de las cuentas;
+        System.out.println(cuenta1.toString());
+        System.out.println(cuenta2.toString());
+
+        //Control de igualdad de saldos;
+        if (cuenta1.getSaldo() == cuenta2.getSaldo()) {
+            System.out.println("El saldo de las cuentas 1 y 2 es el mismo!");
+        }
+
+        //Control de cuenta con mayor saldo;
+        if (cuenta1.getSaldo() > cuenta2.getSaldo()) {
+            System.out.println("La cuenta con mayor saldo es la codigo: 1!");
+        } else if (cuenta1.getSaldo() < cuenta2.getSaldo()) {
+            System.out.println("La cuenta con mayor saldo es la codigo: 2!");
+        }
     }
-    
+
 }
--- Modulo 1/Ejercicios Net Beans/Net Beans Projects/pyCuentaBancaria/src/ar/com/dominio/CuentaBancaria.java
+++ Modulo 1/Ejercicios Net Beans/Net Beans Projects/pyCuentaBancaria/src/ar/com/dominio/CuentaBancaria.java
@@ -11,7 +11,14 @@ public class CuentaBancaria {
     private float saldo;
     private int idCodigo;
 
-    //Metodos:
+    //Constructor: se llama igual que la clase y no tiene tipo de retorno;
+    public CuentaBancaria(String titular, float saldo, int idCodigo) {
+        this.titular = titular; // -> (this.titular = atributo) y (titular = parametro);
+        this.saldo = saldo;
+        this.idCodigo = idCodigo;
+    }
+
+    //Metodos getters and setters:
     public String getTitular() {
         return titular;
     }
@@ -34,6 +41,29 @@ public class CuentaBancaria {
 
     public void setIdCodigo(int idCodigo) {
         this.idCodigo = idCodigo;
+    }
+
+    //Metodo de extraccion y deposito;
+    public void depositar(float monto) {
+        if (monto > 0) {
+            saldo = saldo + monto;
+        } else {
+            System.out.println("Monto ingresado no valido!");
+        }
+    }
+
+    public void extraer(float monto) {
+        if (monto > 0 && saldo >= monto) {
+            saldo = saldo - monto;
+        } else {
+            System.out.println("Saldo insuficiente!");
+        }
+    }
+
+    //Metodo toString -> muestra por la salida estandar o pantalla los datos;
+    @Override
+    public String toString() {
+        return "La cuenta bancaria del titular: " + titular + ", tiene un saldo de: " + saldo + ", y el Codigo de cuenta es: " + idCodigo;
     }
 
 }
